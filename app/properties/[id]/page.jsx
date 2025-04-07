bbc72e54d15b1fa78346d9b6a42df725b7a02053
0xadri <adiber_garcia@hotmail.com>
2025-04-07 10:53:24 +0200

    secure routes + fix css

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -6,15 +6,16 @@ import { FaArrowLeft } from "react-icons/fa";
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
-  const property = await Property.findById(params.id).lean();
+  const currParams = await params;
+  const property = await Property.findById(currParams.id).lean();
   return (
     <>
       <PropertyHeaderImage image={property.images[0]} />
       <section>
-        <div classNameName="container m-auto py-6 px-6">
+        <div className="container m-auto py-6 px-6">
           <Link
             href="/properties"
-            classNameName="text-blue-500 hover:text-blue-600 flex items-center"
+            className="text-blue-500 hover:text-blue-600 flex items-center"
           >
             <FaArrowLeft className="mr-2" /> Back to Properties
           </Link>
