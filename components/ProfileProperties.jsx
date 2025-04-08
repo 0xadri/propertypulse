fd6cde3471ca7a758cb7af07d65fdf8d10b11349
0xadri <adiber_garcia@hotmail.com>
2025-04-08 14:50:18 +0200

    Add Toast

--- components/ProfileProperties.jsx
+++ components/ProfileProperties.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import deleteProperty from "@/app/actions/deleteProperty";
+import { toast } from "react-toastify";
 
 const ProfileProperties = ({ properties: initialProperties }) => {
   const [properties, setProperties] = useState(initialProperties);
@@ -19,6 +20,8 @@ const ProfileProperties = ({ properties: initialProperties }) => {
     );
 
     setProperties(updatedProperties);
+
+    toast.success("Property Deleted Successfully");
   };
 
   return properties.map((property, index) => (
